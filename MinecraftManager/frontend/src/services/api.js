00680fd4caf7458bcbf0d7d4b935e6985774e85d
Gabriel Luezas <130647529+GabrielLuezas@users.noreply.github.com>
2025-12-10 17:49:37 +0100

    cambio front end

--- MinecraftManager/frontend/src/services/api.js
+++ MinecraftManager/frontend/src/services/api.js
@@ -1,9 +1,13 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api";
+// BUSCA LA VARIABLE DE ENTORNO. SI NO EXISTE (EN TU PC), USA LOCALHOST.
+// Nota: Fíjate que quito el "/api" del string por defecto y lo añado después para evitar dobles barras
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const API_URL = `${BASE_URL}/api`;
 
 export const api = {
   getPlayers: async () => {
+    // Esto resultará en: https://tu-backend.onrender.com/api/players
     const response = await axios.get(`${API_URL}/players`);
     return response.data;
   },
